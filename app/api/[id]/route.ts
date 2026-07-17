ba3d90a84abcfde45aca3bc383ada83833ade92c
PBM <PBM@PBMJ02022300270.WORKGORUP>
2026-07-17 19:06:03 +0700

    fix dynamic api

--- app/api/[id]/route.ts
+++ app/api/[id]/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from '@/lib/prisma'
 import { NextResponse } from 'next/server'
 
+export const dynamic = 'force-dynamic'
+
 export async function PUT(req: Request, { params }: { params: Promise<{ id: string }> }) {
   const body = await req.json()
   const { id } = await params
